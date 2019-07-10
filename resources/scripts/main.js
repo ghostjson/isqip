69c992afef177a37ebacc27995a47221894f3fe3
jack <imakhilravindran@gmail.com>
2019-07-10 15:37:47 +0530

    added some more styling and content

---
+++ resources/scripts/main.js
@@ -0,0 +1,37 @@
+let crt_page = 0;
+
+
+let $next_btn = document.getElementById('nextbtn');
+let $back_btn = document.getElementById('backbtn');
+
+let page = [
+    document.getElementById('details-page')
+];
+
+
+window.onload = ()=>{
+    $next_btn.addEventListener('click', ()=>{
+        crt_page++;
+        for(let i=0;i < page.length; i++){
+            if(crt_page == i){
+                page[i].style.display = 'grid';
+            }
+            else{
+                page[i].style.display = "none"; 
+            }
+        }
+    });
+
+    $back_btn.addEventListener('click', ()=>{
+        crt_page--;
+        for(let i=0;i < page.length; i++){
+            if(crt_page == i){
+                page[i].style.display = 'grid';
+            }
+            else{
+                page[i].style.display = "none"; 
+            }
+        }
+    });
+
+}
